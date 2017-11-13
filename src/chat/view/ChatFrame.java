ef04766729307ba1dd6d0d4a15fa26cde5f66387
KaiG24 <31518722+KaiG24@users.noreply.github.com>
2017-11-13 13:45:59 -0700

    Building framework for panel and frame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,26 @@
 package chat.view;
 
-public class ChatFrame
-{
+import chat.controller.ChatController;
+import javax.swing.JFrame;
 
+public class ChatFrame extends JFrame
+{
+	private ChatController appController;
+	private ChatPanel appPane;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame;
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setVisible(true);
+		
+	}
 }
