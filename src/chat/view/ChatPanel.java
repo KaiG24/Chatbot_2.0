6460b10b02bfdfb8b96d2848efe79309a3269c95
KaiG24 <31518722+KaiG24@users.noreply.github.com>
2017-12-05 13:38:37 -0700

    Random commit

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,7 @@
 package chat.view;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
@@ -18,6 +19,7 @@ public class ChatPanel extends JPanel
 	private JTextArea chatArea;
 	private SpringLayout appLayout;
 	private JButton checkerButton;
+	private JScrollPane chatScrollPe;
 	
 	public ChatPanel(ChatController appController)
 	{
@@ -29,7 +31,10 @@ public class ChatPanel extends JPanel
 		inputField = new JTextField(20);
 		appLayout = new SpringLayout();
 		checkerButton = new JButton("Check");
+		chatScrollPane = new JScrollPane();
 		
+		
+		setupScrollPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -42,18 +47,27 @@ public class ChatPanel extends JPanel
 		this.setLayout(appLayout);
 		this.add(chatButton);
 		this.add(inputField);
-		this.add(chatArea);
+		this.add(chatScrollPane);
 		chatArea.setEnabled(false);
 		chatArea.setEditable(false);
 		this.add(checkerButton);
 	}
 	
+	private void setupScrollPane()
+	{
+		chatScrollPane.setViewportView(chatArea);
+		chatScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		chatArea.setLineWrap(true);
+		chatArea.setWrap;
+	}
+	
 	private void  setupLayout()
 	{
-		appLayout.putConstraint(SpringLayout.NORTH, chatButton, 72, SpringLayout.SOUTH, chatArea);
+		appLayout.putConstraint(SpringLayout.NORTH, chatButton, 72, SpringLayout.SOUTH, chatScrollPane);
 		appLayout.putConstraint(SpringLayout.EAST, chatButton, -66, SpringLayout.EAST, this);
-		appLayout.putConstraint(SpringLayout.NORTH, chatArea, 10, SpringLayout.NORTH, this);
-		appLayout.putConstraint(SpringLayout.EAST, chatArea, -68, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.NORTH, chatScrollPane, 10, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatScrollPane, -68, SpringLayout.EAST, this);
 		appLayout.putConstraint(SpringLayout.NORTH, inputField, 0, SpringLayout.NORTH, chatButton);
 		appLayout.putConstraint(SpringLayout.WEST, inputField, 10, SpringLayout.WEST, this);
 	}
